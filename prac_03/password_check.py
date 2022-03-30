8168fee3deec92a0b06fb6e832a22e399efda73d
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-03-31 01:01:46 +0630

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -3,9 +3,26 @@ A simple program for password check
 """
 
 MIN_LENGTH = 6
-password = input("Type a password : ")
-while len(password) < MIN_LENGTH:
-    print("Your password is too short")
+
+
+def main():
+    password = get_password()
+    display_asterisks(password)
+
+
+def get_password():
+    """ Get password and verify minimum length """
     password = input("Type a password : ")
-for x in password:
-    print("*", end="")
+    while len(password) < MIN_LENGTH:
+        print("Your password is too short")
+        password = input("Type a password : ")
+    return password
+
+
+def display_asterisks(password):
+    """ Display asterisks for password """
+    for x in password:
+        print("*", end="")
+
+
+main()
