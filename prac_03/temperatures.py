e266a34c42326c1227ad3be4752d9485171b93de
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-03-31 01:40:29 +0630

    Add docstrings

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -1,15 +1,17 @@
-""" Program for converting Celsius
+""" Programs for converting Celsius
 to Fahrenheit and vice versa
 """
 
 
 def main():
+    """ Get celsius and display fahrenheit """
     celsius = float(input(" Celsius : "))
     fahrenheit = celsius_to_fahrenheit(celsius)
     print("Fahrenheit:", fahrenheit)
 
 
 def celsius_to_fahrenheit(celsius):
+    """ Convert celsius to fahrenheit """
     return celsius * 1.8 + 32.0
 
 
@@ -17,12 +19,14 @@ main()
 
 
 def main():
+    """ Get fahrenheit and display celsius """
     fahrenheit = float(input(" Fahrenheit: "))
     celsius = fahrenheit_to_celsius(fahrenheit)
     print("Celsius:", celsius)
 
 
 def fahrenheit_to_celsius(fahrenheit):
+    """ Convert fahrenheit to celsius """
     return fahrenheit - 32 * 5/9
 
 
