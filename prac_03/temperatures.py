5770d65f7d223871390baa4e6b1054ffb51c644d
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-03-31 01:37:51 +0630

    Add a function that convert Fahrenheit to Celsius

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -6,11 +6,24 @@ to Fahrenheit and vice versa
 def main():
     celsius = float(input(" Celsius : "))
     fahrenheit = celsius_to_fahrenheit(celsius)
-    print(fahrenheit)
+    print("Fahrenheit:", fahrenheit)
 
 
 def celsius_to_fahrenheit(celsius):
     return celsius * 1.8 + 32.0
+
+
+main()
+
+
+def main():
+    fahrenheit = float(input(" Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print("Celsius:", celsius)
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return fahrenheit - 32 * 5/9
 
 
 main()
