a3d90e7d76a99e93e014645e257590dd8044b273
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-03-31 16:17:37 +0630

    Add practical 1 file

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -1,25 +1,9 @@
-import random
-
-
-def main():
-    score = float(input("Enter your score: "))
-    grade = verify_grade(score)
-    print(grade)
-    random_score = random.randint(1, 101)
-    print(random_score)
-    random_grade = verify_grade(random_score)
-    print(random_grade)
-
-
-def verify_grade(score):
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
-        return "Excellent"
-    elif score >= 50:
-        return "Pass"
-    else:
-        return "Bad"
-
-
-main()
+score = float(input("Enter your score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Pass")
+else:
+    print("Bad")
