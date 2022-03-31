df851cee1ec9d70aa78e7544effb21d750e93541
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-03-31 16:19:29 +0630

    Refactor score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,9 +1,18 @@
-score = float(input("Enter your score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter your score: "))
+    grade = determine_grade(score)
+    print(grade)
+
+
+def determine_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+main()
