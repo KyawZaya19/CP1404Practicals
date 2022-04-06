e6b654f37a957af80cd0e1e400fa1538b08a86ab
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-04-06 10:22:28 +0630

    Add docstrings

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -9,16 +9,26 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # False
 # 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
 
-# 1
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+
+""" Change the first element of numbers to "ten" (the string, not the number 10) """
 numbers[0] = "ten"
 
-# 2
+""" Change the last element of numbers to 1 """
 numbers[-1] = 1
 
-# 3
+""" Get all the elements from numbers except the first two (slice) """
 numbers[2:]
 
-# 4
+""" Check if 9 is an element of numbers """
 9 in numbers
 
 
