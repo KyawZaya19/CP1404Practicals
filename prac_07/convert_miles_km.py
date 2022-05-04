425cca0f9d45a51e8145839bb37724c22ac18b30
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-05-04 14:59:18 +0630

    Add docstrings

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -8,26 +8,32 @@ from kivy.properties import StringProperty
 
 
 class MilesConverter(App):
+    """Kivy App for converting miles to kilometres"""
     output_km = StringProperty()
 
     def build(self):
+        """Build the Kivy app from the kv file"""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculate(self, text):
+        """Handle calculation"""
         miles = self.convert_to_float(text)
         self.result(miles)
 
     def handle_increment(self, text, change):
+        """Handle up and down button press, update the new value"""
         miles = self.convert_to_float(text) + change
         self.root.ids.input_miles.text = str(miles)
 
     def result(self, miles):
+        """Calculating result and change it into string"""
         self.output_km = str(miles * 1.60934)
 
     @staticmethod
     def convert_to_float(text):
+        """Convert text to float or 0.0 if invalid."""
         try:
             return float(text)
         except ValueError:
