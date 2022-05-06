691f6ffa706d688db762f4e846286f6822528100
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-05-06 22:57:29 +0630

    Add docstrings

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -5,19 +5,23 @@ from kivy.uix.label import Label
 
 
 class DynamicLabel(App):
+    """Main program for dynamic labels."""
     name = StringProperty()
 
     def __init__(self, **kwargs):
+        """Construct main app."""
         super().__init__(**kwargs)
         self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre", "Jackson"]
 
     def build(self):
+        """Build the Kivy GUI."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file("dynamic_labels.kv")
         self.create_labels()
         return self.root
 
     def create_labels(self):
+        """Create labels."""
         for name in self.names:
             temp_label = Label(text=name)
             self.root.ids.main.add_widget(temp_label)
