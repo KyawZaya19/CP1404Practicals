4112e2baed9920e00b287b6522aaecd6cdbd9f66
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-05-11 11:39:46 +0630

    Make modifications

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -4,12 +4,17 @@
 
 from prac_08.taxi import Taxi
 
-taxi = Taxi("Prius", 100)
-taxi.drive(40)
-print(taxi)
-print(f"Current fare: ${taxi.get_fare()}")
 
-taxi.start_fare()
-taxi.drive(100)
-print(taxi)
-print(f"Current fare: ${taxi.get_fare()}")
+def main():
+    taxi = Taxi("Prius", 100)
+    taxi.drive(40)
+    print(taxi)
+    print(f"Current fare: ${taxi.get_fare()}")
+
+    taxi.start_fare()
+    taxi.drive(100)
+    print(taxi)
+    print(f"Current fare: ${taxi.get_fare()}")
+
+
+main()
