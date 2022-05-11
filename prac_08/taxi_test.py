4a3f4a926c710b3ef309147f6aa7ae524d81610d
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-05-11 11:11:48 +0630

    Add testing for taxi

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,16 @@
+"""
+ Test Taxi
+"""
+
+from prac_08.taxi import Taxi
+
+taxi = Taxi("Prius", 100, 1.23)
+taxi.drive(40)
+print(taxi)
+print(f"Current fare: ${taxi.get_fare()}")
+
+taxi.start_fare()
+taxi.drive(100)
+print(taxi)
+print(f"Current fare: ${taxi.get_fare()}")
+
