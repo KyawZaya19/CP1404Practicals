d6d9c406aaca0197449be8977c6e35b42f1409c1
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-05-11 12:17:19 +0630

    Add docstrings

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -7,14 +7,14 @@ from prac_08.car import Car
 
 
 class UnreliableCar(Car):
-    """ Unreliable car version """
+    """ Unreliable car version. """
     def __init__(self, name, fuel, reliability):
         """ Initialise UnreliableCar"""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
-        """ Drive the car, or not based on reliability """
+        """ Drive the car, or not based on reliability. """
         random_number = random.randint(1, 100)
         if random_number >= self.reliability:
             distance = 0
