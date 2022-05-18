185745add884c17da3d2f1e12456879c64191ffb
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-05-18 12:42:24 +0630

    Make modifications

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -12,26 +12,44 @@ def main():
     # Change to desired directory
     os.chdir('Lyrics/Christmas')
 
-    os.chdir('Lyrics')
+    os.chdir('..')
     for directory_name, subdirectories, filenames in os.walk('.'):
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
 
+        old_directory = os.getcwd()
+        os.chdir(directory_name)
+
         for filename in filenames:
-            full_name = os.path.join(directory_name, filename)
-            new_name = os.path.join(directory_name, get_fixed_filename(filename))
-            os.rename(full_name, new_name)
+            consistent_name = consistent_file_name(filename)
+            os.rename(filename, consistent_name)
+            print(consistent_name)
 
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
+        os.chdir(old_directory)
 
 
-def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
+def consistent_file_name(filename):
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
+    new_name = new_name[0].upper() + new_name[1:]
+    intermediate_name = new_name[0]
+    for index, character in enumerate(new_name[1:]):
+        if character.isupper() and new_name[index - 1].isalpha() and not new_name[index] == "_":
+            intermediate_name += "_" + character
+        else:
+            intermediate_name += character
+
+    consistent_name = intermediate_name[0]
+    for index, character in enumerate(intermediate_name[1:]):
+        if character.islower() and intermediate_name[index] == "_":
+            character = str(character)
+            character = character.upper()
+            consistent_name += character
+        else:
+            consistent_name += character
+
+    return consistent_name
 
 
 main()
